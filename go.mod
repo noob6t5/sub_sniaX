515ba2cb4db5d719960d1af3ab589d2a6e3a5f51
Sangharsha <upadhyayasangharsha@gmail.com>
2024-11-08 21:53:51 +0545

    Function Added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sub_sniaX
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+    "context"
+    "crypto/tls"
+    "flag"
+    "log"
+    "net"
+    "strings"
+    "time"
+
+	"golang.org/x/net/dns/dnsmessage"
+)
+
+
+// Features
+func readDomains(){}
+func enumSubDomain(){}
+func attemptAXFR(){} // Many blovk's it nowadys anyway let's try
+func sniEnumerate(){}
+func checkSNI(){}
+func output2file(){}
+//Main
+func main(){}
+
+//Baki Voli Note2 myself : Don't forgot 2 add todo 
